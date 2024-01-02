8da4ae27dec9efa92f5b5a7a008db5c893112424
stilt0n <mattias.leino@arts.cornish.edu>
2024-01-02 03:57:40 -0500

    add boolean eval object

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -15,9 +15,23 @@ func TestEvalIntegerExpression(t *testing.T) {
 		{"5", 5},
 		{"10", 10},
 	}
-	for _, tt := range tests {
-		evaluated := testEval(tt.input)
-		testIntegerObject(t, evaluated, tt.expected)
+	for _, tc := range tests {
+		evaluated := testEval(tc.input)
+		testIntegerObject(t, evaluated, tc.expected)
+	}
+}
+
+func TestEvalBooleanExpression(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected bool
+	}{
+		{"true", true},
+		{"false", false},
+	}
+	for _, tc := range tests {
+		evaluated := testEval(tc.input)
+		testBooleanObject(t, evaluated, tc.expected)
 	}
 }
 
@@ -38,6 +52,21 @@ func testIntegerObject(t *testing.T, obj object.Object, expected int64) bool {
 
 	if result.Value != expected {
 		t.Errorf("Expected object to have value %d. Got %d", expected, result.Value)
+		return false
+	}
+
+	return true
+}
+
+func testBooleanObject(t *testing.T, obj object.Object, expected bool) bool {
+	result, ok := obj.(*object.Boolean)
+	if !ok {
+		t.Errorf("Expected object to be Boolean. Got %T (%+v)", obj, obj)
+		return false
+	}
+
+	if result.Value != expected {
+		t.Errorf("Expected object to have value %t. Got %t", expected, result.Value)
 		return false
 	}
 
