a9eb9f853e40fcb7e266a8af702a79800ec5d1ba
stilt0n <mattias.leino@arts.cornish.edu>
2024-01-08 23:35:48 -0500

    create basic language server

--- main.go
+++ main.go
@@ -1,21 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"monkey-pl/repl"
-	"os"
-	"os/user"
-)
+import "monkey-pl/server"
 
 func main() {
-	user, err := user.Current()
-
-	if err != nil {
-		fmt.Println("🙊 Oh no! There was an error! See below: 🙊")
-		panic(err)
-	}
-
-	fmt.Printf("🐵 Hello %s! Welcome to the Monkey programming language 🐵\n", user.Username)
-	fmt.Printf("🐵🍌 Try out some commands! Use :exit to exit the repl 🍌🐵\n\n")
-	repl.Start(os.Stdin, os.Stdout)
+	server.Serve()
 }
--- server/server.go
+++ server/server.go
@@ -1,58 +1,136 @@
 package server
 
 import (
+	"bytes"
+	"encoding/json"
 	"errors"
-	"fmt"
 	"io"
+	"log"
 	"monkey-pl/evaluator"
 	"monkey-pl/lexer"
 	"monkey-pl/object"
 	"monkey-pl/parser"
 	"net/http"
 	"os"
+	"strings"
 )
 
-// This should maybe eventually be the real main?
-func Main() {
-	fmt.Printf("Running server on port :%d\n", 5150)
+type EvalRequestBody struct {
+	Code string `json:"code"`
+}
+
+type EvalResponse struct {
+	Result string `json:"result"`
+	// Errors are a valid thing for eval to send back
+	// so I don't want to treat them as server errors
+	// but giving that info will allow me to color them
+	// differently on the front-end
+	IsError bool `json:"isError"`
+}
+
+func Serve() {
+	log.Printf("Running server on port :%d\n", 5150)
 	// This is a sanity check since I'm not really very
 	// familiar with Go's http api yet
 	http.HandleFunc("/", handleRoot)
-	http.HandleFunc("/eval", evaluateMonkeyCode)
+	http.HandleFunc("/eval", handleEvaluate)
 	err := http.ListenAndServe(":5150", nil)
 	if errors.Is(err, http.ErrServerClosed) {
-		fmt.Println("The server is shutting down...")
+		log.Println("The server is shutting down...")
 		return
 	}
 	if err != nil {
-		fmt.Printf("Error: %s\n", err)
+		log.Printf("Error: %s\n", err)
 		os.Exit(1)
 	}
 }
 
 func handleRoot(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("This is the root route!")
+	log.Println("This is the root route!")
 	io.WriteString(w, "Test")
 }
 
-// TODO: Make this function actually read and send data
-func evaluateMonkeyCode(w http.ResponseWriter, r *http.Request) {
-	code := "let standIn = 10; standIn;"
+func handleEvaluate(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		return
+	}
+	parsedBody := EvalRequestBody{}
+	err := fromJson(r.Body, &parsedBody)
+	if err != nil {
+		sendErr(w, err, 400)
+		return
+	}
+	code := parsedBody.Code
 	env := object.NewEnvironment()
 	lex := lexer.New(code)
 	prs := parser.New(lex)
 	program := prs.ParseProgram()
 
+	response := EvalResponse{}
 	if len(prs.Errors()) != 0 {
-		fmt.Println("There was an error that should be sent by response writer")
+		response.Result = strings.Join(prs.Errors(), "\n")
+		response.IsError = true
+		sendJson(w, func() (interface{}, error) {
+			return response, nil
+		})
 		return
 	}
-
 	evaluated := evaluator.Eval(program, env)
-	if evaluated != nil {
-		fmt.Println(evaluated.Inspect())
+	// TODO: Perhaps this should actually return a NULL object.Object
+	if evaluated == nil {
+		response.Result = "NULL"
+	} else {
+		response.Result = evaluated.Inspect()
+	}
+	response.IsError = false
+	sendJson(w, func() (interface{}, error) {
+		return response, nil
+	})
+}
+
+func fromJson[T any](body io.Reader, target T) error {
+	buf := new(bytes.Buffer)
+	buf.ReadFrom(body)
+	return json.Unmarshal(buf.Bytes(), &target)
+}
+
+func setJsonHeader(w http.ResponseWriter) {
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+}
+
+func sendJson[T any](w http.ResponseWriter, withData func() (T, error)) {
+	setJsonHeader(w)
+
+	data, serverErr := withData()
+	if serverErr != nil {
+		w.WriteHeader(500)
+		serverErrJson, err := json.Marshal(&serverErr)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		w.Write(serverErrJson)
 		return
 	}
 
-	fmt.Println("NULL")
+	dataJson, err := json.Marshal(&data)
+	if err != nil {
+		log.Println(err)
+		w.WriteHeader(500)
+		return
+	}
+
+	w.Write(dataJson)
+}
+
+func sendErr(w http.ResponseWriter, err error, code int) {
+	sendJson(w, func() (interface{}, error) {
+		errorMessage := struct {
+			Err string
+		}{
+			Err: err.Error(),
+		}
+		w.WriteHeader(code)
+		return errorMessage, nil
+	})
 }
